84f934ac83abc5d1d1da7af690a014b78435980d
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2021-09-30 14:40:54 +0200

    Final Commit2

--- src/Main.java
+++ src/Main.java
@@ -17,3 +17,9 @@ public class Main
         th2.start();
     }
 }
+
+
+/* Die JVM welchselt die Thread schnell, aber nicht schnell genug das es zwischen den 20 Ausgaben der Klassen wechselt
+    deswegen werden die Zahlen und die Uhrzeiten geblockt ausgegeben und nicht durchmischt.
+    Erst bei sehr vielen Ausgaben kommt die JVm dazu die Threads zu wechseln und die Ausgaben vermischen.
+ */
