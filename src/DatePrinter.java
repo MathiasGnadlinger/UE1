7a14e2d29723e7ef75b2db164fca7def66ef8cb6
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2021-09-30 14:31:10 +0200

    DatePrinter implemented

---
+++ src/DatePrinter.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class DatePrinter implements Runnable
+{
+    @Override
+    public void run()
+    {
+        Date date = new Date();
+        for (int i = 0; i <= 200000; i++)
+        {
+            System.out.println("Current Date is: " +date);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main
 {
     public static void main(String[] args) {
-        System.out.println("odnfosnfds");
+        System.out.printf("*** Welcome ***\n");
+
+        DatePrinter dt1 = new DatePrinter();
+        Thread th1 = new Thread(dt1);
+
+        CountPrinter cp1 = new CountPrinter();
+        Thread th2 = new Thread(cp1);
+
+        th1.start();
+        th2.start();
+
     }
 
 
